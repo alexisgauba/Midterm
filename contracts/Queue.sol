2051f719b89b05fd98251e183d853712c973b063
Vyacheslav <tsarsandrbasileus@berkeley.edu>
2017-10-24 22:35:21 -0700

    Queue progress

--- contracts/Queue.sol
+++ contracts/Queue.sol
@@ -10,50 +10,100 @@ pragma solidity ^0.4.15;
 contract Queue {
 	/* State variables */
 	uint8 size = 5;
+	uint8 elements; //size will be for the size of the whole array, elements for how much of it is filled
+	uint256 time_limit = 1 << 8; //time limit, equal to 2^8 = 256 
+	uint256 front_time;
 	// YOUR CODE HERE
-
+	address[] queue;
+	bool lock;
 	/* Add events */
 	// YOUR CODE HERE
-
+	event AddedClient(address Client, uint256 timestamp);
+	event PurchaseCompleted(address Client);
 	/* Add constructor */
 	// YOUR CODE HERE
-
+	
+	function Queue () {
+		queue = new address[](size);
+		lock = false;
+		elements = 0;
+	}
+	
+	function resize() constant {
+		if (elements == size - 1) {
+			uint8 nSize = size * 2;
+			assert(size == 0 || nSize / 2 == size);
+			address[] memory newQueue= new address[](nSize);
+			uint i;
+			for (i = 0; i < size; i++) {
+				newQueue[i] = queue[i];
+			}
+			for (; i < nSize; i++) {
+				newQueue[i] = 0x0;
+			}
+			queue = newQueue;
+			size = nSize;
+		} 
+	}
+	
 	/* Returns the number of people waiting in line */
 	function qsize() constant returns(uint8) {
-		// YOUR CODE HERE
+		return elements;
 	}
 
 	/* Returns whether the queue is empty or not */
 	function empty() constant returns(bool) {
-		// YOUR CODE HERE
+		return elements == 0;
 	}
 	
 	/* Returns the address of the person in the front of the queue */
 	function getFirst() constant returns(address) {
-		// YOUR CODE HERE
+		return queue[0];
 	}
 	
 	/* Allows `msg.sender` to check their position in the queue */
 	function checkPlace() constant returns(uint8) {
-		// YOUR CODE HERE
+		for (uint8 i = 0; i < elements; i++) {
+			if (queue[i] == msg.sender) {
+				return i;
+			}
+		}
+		return 255; //for error
 	}
+	
+	
 	
 	/* Allows anyone to expel the first person in line if their time
 	 * limit is up
 	 */
 	function checkTime() {
-		// YOUR CODE HERE
+		if (now - front_time >= time_limit) {
+			lock = false;
+		} else {
+			lock = true;
+		}
 	}
 	
 	/* Removes the first person in line; either when their time is up or when
 	 * they are done with their purchase
 	 */
 	function dequeue() {
-		// YOUR CODE HERE
+		assert(empty() == false);
+		checkTime();
+		if (!lock ) {
+				for (uint i = 0; i < elements - 1; i++) {
+					queue[i] = queue[i + 1]; 
+				}
+			elements--;
+			front_time = now;
+		}
+		
 	}
 
 	/* Places `addr` in the first empty position in the queue */
 	function enqueue(address addr) {
-		// YOUR CODE HERE
+		resize();
+		queue[elements++] = addr;
+		AddedClient(addr, now);
 	}
 }
